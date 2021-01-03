97b8df7267354a150d1f30561c869c94935632fe
xjdm <1121245860@qq.com>
2021-01-04 00:33:03 +0800

    双栈模拟队列

---
+++ src/com/idstaa/lg/stage4/CQueue.java
@@ -0,0 +1,34 @@
+package com.idstaa.lg.stage4;
+
+import java.util.Stack;
+
+/**
+ * @author chenjie
+ * @date 2021/1/2 22:43
+ */
+public class CQueue {
+    private Stack<Integer> stackA ;
+    private Stack<Integer> stackB ;
+    public CQueue() {
+        this.stackA = new Stack();
+        this.stackB = new Stack();
+    }
+
+    public void appendTail(int value) {
+        //在此处写入代码
+        while(!stackB.isEmpty()){
+            stackA.push(stackB.pop());
+        }
+        stackA.push(value);
+    }
+
+    public int deleteHead() {
+        while(!stackA.isEmpty()){
+            stackB.push(stackA.pop());
+        }
+        if(stackB.isEmpty()){
+            return -1;
+        }
+        return stackB.pop();
+    }
+}
