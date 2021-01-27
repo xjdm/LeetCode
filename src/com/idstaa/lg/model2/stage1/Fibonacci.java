ca1e60bfea4d6bb6dafdde864463e9fb25368ae8
xjdm <1121245860@qq.com>
2021-01-27 12:03:50 +0800

    斐波那契

---
+++ src/com/idstaa/lg/model2/stage1/Fibonacci.java
@@ -0,0 +1,44 @@
+package com.idstaa.lg.model2.stage1;
+
+/**
+ * @author chenjie
+ * @date 2021/1/27 11:40
+ *
+ * 2/4 斐波那契数
+ * 斐波那契数，通常用 F(n) 表示，形成的序列称为斐波那契数列。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ *
+ * F(0) = 0, F(1) = 1
+ * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+ * 给定 N，计算 F(N)。
+ *
+ * 示例 1：
+ * 输入：2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1.
+ *
+ * 示例 2：
+ * 输入：3
+ * 输出：2
+ * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2.
+ *
+ * 示例 3：
+ * 输入：4
+ * 输出：3
+ * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3.
+ *
+ * 提示：
+ * 0 ≤ N ≤ 30
+ */
+public class Fibonacci {
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci();
+        int fib = fibonacci.fib(2);
+        System.out.println(fib);
+    }
+    public int fib(int N) {
+        // 在此处写入代码
+        if(N==1) return 1;
+        if(N ==0) return 0;
+        return  fib(N-1)+fib(N-2);
+    }
+}
