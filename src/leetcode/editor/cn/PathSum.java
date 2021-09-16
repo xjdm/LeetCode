6efd49beb1f13a5c3c5c32480676afdc50df1466
chenjie <chenjie@erwan365.com>
2021-09-16 16:28:15 +0800

    112_路径总和

---
+++ src/leetcode/editor/cn/PathSum.java
@@ -0,0 +1,38 @@
+package leetcode.editor.cn;
+//Java：路径总和
+public class PathSum{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        // TO TEST
+    }
+}
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null){
+            return false;
+        }
+        if(root.left==null && root.right==null){
+            return targetSum-root.val==0;
+        }
+        boolean leftHasPathSum = hasPathSum(root.left, targetSum - root.val);
+        boolean rightHashPathSum = hasPathSum(root.right, targetSum - root.val);
+        return  leftHasPathSum || rightHashPathSum;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
